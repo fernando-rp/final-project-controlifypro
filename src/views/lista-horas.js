45098bd0852980a69816de64747144f5636e115b
fernandor-p <fernandor.p@hotmail.com>
2021-07-18 17:02:51 -0400

    ajuste lista horas

--- src/views/lista-horas.js
+++ src/views/lista-horas.js
@@ -6,29 +6,29 @@ import Swal from "sweetalert2";
 import Calendar from 'react-calendar';
 
 
-const ListaHoras = ()=>{
-    const {store, actions}= useContext(Context);
-    const {actividades,proyectos,actividad} = store;
+const ListaHoras = () => {
+    const { store, actions } = useContext(Context);
+    const { actividades, proyectos, actividad } = store;
     const [value, onChange] = useState(new Date());
-    const {id}=useParams();
-    const history= useHistory();
+    const { id } = useParams();
+    const history = useHistory();
 
-    const [data,setData]=useState(null)
+    const [data, setData] = useState(null)
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        actions.getActividadById('/actividades',id)
-        
-    },[])
+        actions.getActividadById('/actividades', id)
 
-    const formatDate = (date)=>{
-        date=date.getFullYear()+"-"+(parseFloat(date.getMonth())+1)+"-"+date.getDate();
+    }, [])
+
+    const formatDate = (date) => {
+        date = date.getFullYear() + "-" + (parseFloat(date.getMonth()) + 1) + "-" + date.getDate();
         setData({
             ...data,
             fecha_inicio: date
         })
     }
-    
+
     const confirmacion_saved = () => {
 
         Swal.fire({
@@ -37,35 +37,35 @@ const ListaHoras = ()=>{
             title: 'Actividad Editada',
             showConfirmButton: false,
             timer: 1300
-          })
+        })
     }
 
-    const projectName= (e)=>{
+    const projectName = (e) => {
         console.log(e.target.value)
         console.log(e.target.name)
         actions.handleChangeActividad(e)
-        
+
     }
 
 
-    return(
+    return (
         <div className="container mt-4">
             <div className="row">
-            <div className="col-4 fs-5 bg-primary text-light">Agregar Horas</div>
+                <div className="col-4 fs-5 bg-primary text-light">Agregar Horas</div>
             </div>
             <form onSubmit={(e) => {
-                            e.preventDefault();
-                            actions.updateActividad("/actividades", id, history);
-                        
-                            
-                        }}> 
-            <div className="row border boder-primary mb-4">
-                <div className="col-12">
-                    <div className="row g-3 mt-3">
+                e.preventDefault();
+                actions.updateActividad("/actividades", id, history);
+
+
+            }}>
+                <div className="row border boder-primary mb-4">
+                    <div className="col-12">
+                        <div className="row g-3 mt-3">
                             <div className="col-md-6 mb-3">
 
-                                <label for="floatingTextarea">Proyecto</label> <br/>
-                                <select class="form-select" aria-label="" name="proyecto_id" onChange={(e) => { projectName(e) }}>
+                                <label for="floatingTextarea">Proyecto</label> <br />
+                                <select className="form-select" aria-label="" name="proyecto_id" onChange={(e) => { projectName(e) }}>
                                     <option selected>Seleccionar proyecto</option>
                                     {!!proyectos &&
                                         proyectos.map((proyecto) => {
@@ -76,10 +76,10 @@ const ListaHoras = ()=>{
                                 </select>
                             </div>
 
-                           <div className="col-md-6 mb-3">
-                           <label for="floatingTextarea">Actividad</label> <br/>
-                                <select class="form-select" aria-label="" name="proyecto_id" onChange={(e)=>{projectName(e)}}>
-                                <option selected>Seleccionar actividad</option>
+                            <div className="col-md-6 mb-3">
+                                <label for="floatingTextarea">Actividad</label> <br />
+                                <select class="form-select" aria-label="" name="proyecto_id" onChange={(e) => { projectName(e) }}>
+                                    <option selected>Seleccionar actividad</option>
                                     {!!actividades &&
                                         actividades.map((actividad) => {
                                             return (
@@ -87,31 +87,23 @@ const ListaHoras = ()=>{
                                         })
                                     }
                                 </select>
-                           </div>
+                            </div>
 
-                        <div className="col-md-6 mb-3 ">
-                             <label for="presupuesto" className="form-label">Fecha</label>
+                            <div className="col-md-6 mb-3 ">
+                                <label for="presupuesto" className="form-label">Fecha</label>
+                                <input type="date" className="form-control mb-3 col-8" />
+                            </div>
 
-                                <div className="col-md-8">
-                                    <Calendar
-                                        name="fecha_inicio"
-                                        onChange={onChange}
-                                        onClickDay={formatDate}
-                                        value={value}
-                                    />
-                                </div>
-                        </div>
-
-                        <div className="col-md-6 mb-3">
-                            <label for="floatingTextarea" >Descripción</label>
-                            <textarea
-                            rows="4"                         
-                            name="descripcion"
-                            className="form-control mb-3 col-8" 
-                            placeholder="" 
-                            id="floatingTextarea"         
-                            >
-                            </textarea>
+                            <div className="col-md-6 mb-3">
+                                <label for="floatingTextarea" >Descripción</label>
+                                <textarea
+                                    rows="4"
+                                    name="descripcion"
+                                    className="form-control mb-3 col-8"
+                                    placeholder=""
+                                    id="floatingTextarea"
+                                >
+                                </textarea>
 
                                 <div className="row">
                                     <div className="col-4">
@@ -135,16 +127,16 @@ const ListaHoras = ()=>{
 
                                 </div>
 
-                            <label for="usuario" className="form-label">Usuario</label>
-                            <input 
-                            name="usuario_id"
-                            type="text" 
-                            className="form-control col-5" 
-                            id="inputusuario" 
-                            />
-                        </div>
+                                <label for="usuario" className="form-label">Usuario</label>
+                                <input
+                                    name="usuario_id"
+                                    type="text"
+                                    className="form-control col-5"
+                                    id="inputusuario"
+                                />
+                            </div>
 
-                        <div className="col-md-12 mx-auto">
+                        {/* <div className="col-md-12 mx-auto">
                         <label className="form-check-label mb-3" for="inlineFormCheck">Estado</label>
                             <div className="form-check">
                                 <div class="form-check form-check-inline">
@@ -156,7 +148,7 @@ const ListaHoras = ()=>{
                                     <label className="form-check-label p-0" for="inlineRadioInactive">Inactivo</label>
                                 </div>      
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                     
                 </div> 
