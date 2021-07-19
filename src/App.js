f3a9b6c53cae79ae5de5687f39a5e6d650358810
Anthonny Tineo <anthonnytineo@gmail.com>
2021-07-19 01:34:59 -0400

    Creacion del dashboard

--- src/App.js
+++ src/App.js
@@ -17,10 +17,11 @@ import NavbarJefe from './components/navbarjefe'
 import Navbarproyect from './components/navbarproyecto'
 
 import ListaUsuarios from "./views/ListaUsuarios";
-
 import { RegistroUsuario } from "./views/RegistroUsuario";
-
 import { EdicionUsuario } from "./views/EdicionUsuario";
+
+import { DashboardAdministrador } from "./views/Dashboard-administrador";
+
 
 import { Login } from "./views/Login";
 import Error404 from "./views/error404";
@@ -31,79 +32,82 @@ import injectContext from "./store/appContext";
 const App = () => {
 
     return (
-        <BrowserRouter>
-            <Switch>
+      <BrowserRouter>
+        <Switch>
+          {/* landingPage */}
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/">
+            <Navbar />
+            <Home />
+          </Route>
+          <Route exact path="/Login">
+            <Login />
+          </Route>
 
-            
-            {/* landingPage */}
-                <Route exact path="/contact">
-                    <Contact />
-                </Route>
-                <Route exact path="/">
-                    <Navbar />
-                    <Home/>
-                </Route>
-                <Route exact path="/Login">
-                    <Login />
-                </Route>
+          {/* proyectos */}
+          <Route exact path="/listado-proyectos">
+            <NavbarJefe />
+            <ListadoProyectos />
+          </Route>
+          <Route exact path="/registro-edicion-proyecto/:id">
+            <NavbarJefe />
+            <EdicionProyecto />
+          </Route>
+          <Route exact path="/registro-proyectos">
+            <NavbarJefe />
+            <RegistroProyecto />
+          </Route>
 
-            {/* proyectos */}
-                <Route exact path="/listado-proyectos">
-                    <NavbarJefe />
-                    <ListadoProyectos />
-                </Route>
-                <Route exact path="/registro-edicion-proyecto/:id">
-                    <NavbarJefe />
-                    <EdicionProyecto />
-                </Route>
-                <Route exact path="/registro-proyectos">
-                    <NavbarJefe />
-                    <RegistroProyecto />
-                </Route>
+          {/* actividades */}
+          <Route exact path="/listado-actividades">
+            <NavbarJefe />
+            <ListadoActividades />
+          </Route>
+          <Route exact path="/registro-edicion-actividad/:id">
+            <NavbarJefe />
+            <EdicionActividad />
+          </Route>
+          <Route exact path="/listado-actividades/registro-actividad">
+            <NavbarJefe />
+            <RegistroActividad />
+          </Route>
 
+          {/* Usuarios */}
+          <Route exact path="/lista-usuarios">
+            <NavbarJefe />
+            <ListaUsuarios />
+          </Route>
+          <Route exact path="/EdicionUsuario/:id">
+            <NavbarJefe />
+            <EdicionUsuario />
+          </Route>
+          <Route exact path="/RegistroUsuario">
+            <NavbarJefe />
+            <RegistroUsuario />
+          </Route>
 
-            {/* actividades */}
-                <Route exact path="/listado-actividades">
-                    <NavbarJefe />
-                    <ListadoActividades />
-                </Route>
-                <Route exact path="/registro-edicion-actividad/:id">
-                    <NavbarJefe />
-                    <EdicionActividad />
-                </Route>
-                <Route exact path="/listado-actividades/registro-actividad">
-                    <NavbarJefe />
-                    <RegistroActividad />
-                </Route>
-            
-            {/* Usuarios */}
-                <Route exact path="/lista-usuarios">
-                    <NavbarJefe />
-                    <ListaUsuarios />
-                </Route>
-                <Route exact path="/EdicionUsuario/:id">
-                    <NavbarJefe />
-                    <EdicionUsuario />
-                </Route>
-                <Route exact path="/RegistroUsuario">
-                    <NavbarJefe />
-                    <RegistroUsuario />
-                </Route>
+          {/* Horas */}
+          <Route exact path="/lista-horas">
+            <NavbarJefe />
+            <ListaHoras />
+          </Route>
 
-            {/* Horas */}
-                <Route exact path="/lista-horas">
-                    <NavbarJefe />
-                    <ListaHoras />
-                </Route>
+          {/* Dashboard */}
+          <Route exact path="/Dashboard-administrador">
+            <NavbarJefe />
+            <DashboardAdministrador />
+          </Route>
 
-            {/* Otros */}
-                <Route>
-                    <Error404 />
-                </Route>
-            </Switch>
-            {/* <Footer /> */}
-        </BrowserRouter>
-    )
+          {/* Otros */}
+          <Route>
+            <Error404 />
+          </Route>
+        </Switch>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    );
 }
 
 export default injectContext(App);
